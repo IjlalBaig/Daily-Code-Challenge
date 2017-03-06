739e45d7ca0ee41b271edc4027d7b751a1447b00
Muhammad Ijlal Baig <ijlalbaig92@gmail.com>
2017-03-06 18:35:02 +0100

    Day 1 Run Arguments Test

---
+++ Day1/day1_argc_argv.cc
@@ -0,0 +1,10 @@
+#include<iostream>
+using namespace std;
+
+int main(int argc, char const *argv[]) {
+	for (int i = 1; i < argc; i++) {
+		std::cout << "argv " << i << ": " << argv[i] << endl;
+	}
+
+	return 0;
+}
