f1b6883919506b506b9c3d43a88e7e26cd7afe4e
Muhammad Ijlal Baig <ijlalbaig92@gmail.com>
2017-03-07 14:32:23 +0100

    Pointers Test

--- Day2/Pointers.cc
+++ Day2/Pointers.cc
@@ -2,15 +2,15 @@
 using namespace std;
 
 int main(int argc, char const *argv[]) {
-  int i[3] = {1, 2, 3};
-  int *ptr;
-  std::cout << "Size of Array in Bytes: " << sizeof(i) << '\n';
-  ptr = i;
-  std::cout << "Array Address: " << ptr << '\n';
-  std::cout << "Next Address: " << ptr + 1 << '\n';
-  std::cout << "Next Next Address: " << ptr + 2 << '\n';
-  std::cout << "Value Stored at Last Index: " << *(ptr + 2) << '\n';
-  std::cout << "Value Stored at Next Index: " << *(ptr + 3) << '\n';
+    int i[3] = {1, 2, 3};
+    int *ptr;
+    std::cout << "Size of Array in Bytes: " << sizeof(i) << '\n';
+    ptr = i;
+    std::cout << "Array Address: " << ptr << '\n';
+    std::cout << "Next Address: " << ptr + 1 << '\n';
+    std::cout << "Next Next Address: " << ptr + 2 << '\n';
+    std::cout << "Value Stored at Last Index: " << *(ptr + 2) << '\n';
+    std::cout << "Value Stored at Next Index: " << *(ptr + 3) << '\n';
+    return 0;
 
-  return 0;
 }
