7c7bb056b694b3dcb79d3563c14e16c16312587f
Muhammad Ijlal Baig <ijlalbaig92@gmail.com>
2017-03-15 21:10:19 +0100

    Namespaces Test

--- Day7/Namespaces.cc
+++ Day7/Namespaces.cc
@@ -3,18 +3,25 @@
 namespace ML{
     std::string name = "Machine Learning";
     float weight[5] = {1.0, 2.0, 3.0, 4.0, 5.0};
-};
+}
 
 namespace CV{
     std::string name = "Computer Vision";
     int weight = 24;
-};
+}
+namespace{
+    std::string name = "Nameless";
+    int weight = 199;
+}
 int main(int argc, char const *argv[]) {
     std::cout << "ML namespace: " << ML::name << '\n';
     std::cout << "ML namespace: " << ML::weight[1] << '\n';
 
     std::cout << "CV namespace: " << CV::name << '\n';
     std::cout << "CV namespace: " << CV::weight << '\n';
+
+    std::cout << "empty namespace: " << name << '\n';
+    std::cout << "empty namespace: " << weight << '\n';
 
     return 0;
 }
